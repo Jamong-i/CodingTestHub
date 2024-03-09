fc3907f16a78f2aa6b2208954e44f5588aa745b9
Jamong <smkim961211@gmail.com>
2024-03-09 22:22:32 +0900

    [level 0] Title: 문자열 출력하기, Time: 55.12 ms, Memory: 12.2 MB -BaekjoonHub

--- 프로그래머스/0/181952. 문자열 출력하기/문자열 출력하기.swift
+++ 프로그래머스/0/181952. 문자열 출력하기/문자열 출력하기.swift
@@ -1,6 +1,8 @@
 import Foundation
 
-let str: String = readLine()!
+guard let str: String = readLine() else {
+    fatalError("입력 없음")
+}
 
 guard 1...1_000_000 ~= str.count else {
     fatalError("제한사항: 1과 1,000,000사이의 문자열 길이가 아님")
