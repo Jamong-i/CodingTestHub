14f9d846b9c05876c49c0e4838d531a7ae6583af
Jamong <smkim961211@gmail.com>
2024-03-09 22:33:56 +0900

    [level 0] Title: a와 b 출력하기, Time: 5.68 ms, Memory: 13.3 MB -BaekjoonHub

--- 프로그래머스/0/181951. a와 b 출력하기/a와 b 출력하기.swift
+++ 프로그래머스/0/181951. a와 b 출력하기/a와 b 출력하기.swift
@@ -1,24 +1,8 @@
 import Foundation
 
-// 제한사항을 포함한 출력함수 생성
-func printNum(_ a: Int, _ b: Int){
-    guard (-100_000...100_000).contains(a) && (-100_000...100_000).contains(b) else {
-        fatalError("제한사항 : -100,000 이상 100,000 이하의 수를 받음")
-    }
-    
-    print("a =" , a)
-    print("b =" , b)
+guard let input = readLine()?.split(separator: " ").compactMap({ Int($0) }), input.count == 2 else {
+    fatalError("잘못된 입력")
 }
 
-// 입력값 공백으로 분류
-guard let inputNum = readLine()?.split(separator: " ").compactMap({ Int($0) }) else {
-    fatalError("입력값 없음")
-}
-
-// 입력값 갯수 확인
-guard inputNum.count == 2 else {
-    fatalError("입력값이 두개가 아님")
-}
-
-
-printNum(inputNum[0], inputNum[1])
+print("a = \(input[0])")
+print("b = \(input[1])")
