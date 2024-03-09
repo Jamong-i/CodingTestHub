3c4b02a5d1c21d1f144e73a776217b7af58c25ae
Jamong <smkim961211@gmail.com>
2024-03-09 20:55:23 +0900

    [level 0] Title: 두 수의 나눗셈, Time: 0.02 ms, Memory: 16.2 MB -BaekjoonHub

--- 프로그래머스/0/120806. 두 수의 나눗셈/두 수의 나눗셈.swift
+++ 프로그래머스/0/120806. 두 수의 나눗셈/두 수의 나눗셈.swift
@@ -4,5 +4,5 @@ func solution(_ num1:Int, _ num2:Int) -> Int {
     guard 1...100 ~= num1 && 1...100 ~= num2 else {
         fatalError("제한사항: 1과 100사이의 값이 아님")
     }
-    return Int(Double(num1) / Double(num2) * Double(1000))
+    return Int(Double(num1) / Double(num2) * 1000)
 }
