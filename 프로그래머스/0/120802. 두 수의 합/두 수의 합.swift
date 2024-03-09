1dfc5ac39677266646d29c0dae53cfbda1d818b6
Jamong <smkim961211@gmail.com>
2024-03-09 20:40:46 +0900

    [level 0] Title: 두 수의 합, Time: 0.02 ms, Memory: 16.1 MB -BaekjoonHub

--- 프로그래머스/0/120802. 두 수의 합/두 수의 합.swift
+++ 프로그래머스/0/120802. 두 수의 합/두 수의 합.swift
@@ -1,12 +1,9 @@
 import Foundation
 
-func solution(_ num1: Int, _ num2: Int) -> Int {
-    // 제한 사항
-    guard (-50_000...50000).contains(num1) && (-50_000...50000).contains(num2) else {
-        fatalError("제한사항: -500,000 이상 500,000 이하의 수를 받음")
+func solution(_ num1:Int, _ num2:Int) -> Int {
+    guard -50000...50000 ~= num1 && -50000...50000 ~= num2 else { 
+        fatalError("제한사항: -50,000과 50,000 사이의 값이 아님")
     }
     
-    // 문제 풀이
-    var answer: Int = num1 + num2
-    return answer
+    return num1 + num2
 }
