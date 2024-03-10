b29c5bddf5c40098e7045fc5f31dc351453239e5
Jamong <smkim961211@gmail.com>
2024-03-10 11:07:30 +0900

    [level 0] Title: 나머지 구하기, Time: 0.02 ms, Memory: 16.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120810. 나머지 구하기/나머지 구하기.swift
@@ -0,0 +1,8 @@
+import Foundation
+
+func solution(_ num1:Int, _ num2:Int) -> Int {
+    guard 1...100 ~= num1 && 1...100 ~= num2 else {
+        fatalError("제한사항: 1과 100사이의 값이 아님")
+    } 
+    return num1 % num2
+}
