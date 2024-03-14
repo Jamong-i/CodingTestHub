0a56490fd208f4c0fbd46dd84a17976928933c9c
Jamong <smkim961211@gmail.com>
2024-03-14 16:52:14 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.03 ms, Memory: 16.4 MB -BaekjoonHub

--- 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.swift
+++ 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.swift
@@ -1,5 +1,19 @@
 import Foundation
 
 func solution(_ num_list:[Int]) -> [Int] {
-    return []
+    guard 1...100 ~= num_list.count else {
+        fatalError("제한사항: 1부터 100까지의 리스트의 길이가 아님")
+    }
+    guard num_list.allSatisfy( { 0...1_000 ~= $0 }) else {
+        fatalError("제한사항: 0부터 1,000까지의 원소의 값이 아님")
+    }
+    
+    var odd: Int = 0
+    var even: Int = 0
+    
+    for i in num_list {
+        i % 2 != 0 ? ( odd += 1 ) : ( even += 1 )
+    }
+    
+    return [even, odd]
 }
