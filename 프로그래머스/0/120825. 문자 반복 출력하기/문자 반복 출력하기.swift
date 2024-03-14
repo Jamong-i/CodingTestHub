2793652d4117846346b1b323fc5d7fc1b6d8b5d3
Jamong <smkim961211@gmail.com>
2024-03-14 17:07:55 +0900

    [level 0] Title: 문자 반복 출력하기, Time: 0.03 ms, Memory: 16.4 MB -BaekjoonHub

--- 프로그래머스/0/120825. 문자 반복 출력하기/문자 반복 출력하기.swift
+++ 프로그래머스/0/120825. 문자 반복 출력하기/문자 반복 출력하기.swift
@@ -9,15 +9,11 @@ func solution(_ my_string: String, _ n: Int) -> String {
         fatalError("제한사항: 2부터 10까지의 정수가 아님")
     }
     
-    // guard my_string.rangeOfCharacter(from: CharacterSet.uppercaseLetters.union(CharacterSet.lowercaseLetters.inverted)) == nil else {
-    //     fatalError("제한사항: 영어 대소문자로만 이루어져 있어야 함")
-    // }
-    
     var result: String = ""
     
     for char in my_string {
         result += String(repeating: String(char), count: n)
     }
     
-    return result
+    return result 
 }
